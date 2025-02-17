61193b9ff6d091280b78d2351be8040e7faa04f4
Singiza <Singiza28.com>
2025-02-17 16:36:13 +0200

    Initial commit

---
+++ src/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Layout.css"; // Import CSS file for styling
+
+const Layout = () => {
+  return (
+    <div className="container">
+      <div className="box box1">1</div>
+      <div className="box box2">2</div>
+      <div className="box box3">3</div>
+      <div className="box box4a">4a</div>
+      <div className="box box4b">4b</div>
+    </div>
+  );
+};
+
+export default Layout;
